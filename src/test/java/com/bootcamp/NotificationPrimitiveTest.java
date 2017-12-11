4b89f5b26e537d3a98256aeb0db89534b1e152b2
Baom <bellomouhoutassim@gmail.com>
2017-12-11 15:46:21 +0100

    Correction des problèmes d'accès

--- src/test/java/com/bootcamp/NotificationPrimitiveTest.java
+++ src/test/java/com/bootcamp/NotificationPrimitiveTest.java
@@ -6,6 +6,7 @@
 package com.bootcamp;
 
 import com.bootcamp.Classes.NotificationGn;
+import com.bootcamp.Classes.NotificationInput;
 import com.bootcamp.services.NotificationService;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -17,6 +18,7 @@ import java.lang.reflect.Type;
 import java.sql.SQLException;
 import java.util.List;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 /**
  *
@@ -25,59 +27,58 @@ import org.testng.annotations.Test;
 public class NotificationPrimitiveTest {
 
     NotificationService notificationService = new NotificationService();
-//    @Test
-//    public void generateNotificationTest() throws FileNotFoundException, SQLException, IOException {
-//        NotificationInput input = new NotificationInput();
-//        input.setAction("new_project");
-//        input.setEntityId(2);
-//        input.setEntityType("PROJET");
-//        input.setTitre("la ville numerique de seme city");
-//
-//        Assert.assertTrue(notificationService.checkEventAndgenerateNotification(input));
-//
-//        NotificationInput input1 = new NotificationInput();
-//        input1.setAction("update_project_phase");
-//        input1.setEntityId(1);
-//        input1.setEntityType("PROJET");
-//        input1.setTitre("interconnexion des ministeres");
-//        input1.setAttributName("phase actuelle");
-//        input1.setLastVersion("pre_etude");
-//        input1.setCurrentVersion("financement");
-//
-//        Assert.assertTrue(notificationService.checkEventAndgenerateNotification(input1));
-//    }
+   @Test
+   public void generateNotificationTest() throws FileNotFoundException, SQLException, IOException {
+       NotificationInput input = new NotificationInput();
+       input.setAction("new_project");
+       input.setEntityId(2);
+       input.setEntityType("PROJET");
+       input.setTitre("la ville numerique de seme city");
 
-    @Test
-    public void generateNotificationTest() throws FileNotFoundException, SQLException, IOException {
-        String chaine = readFileToString();
-        Gson gson = new Gson();
-        Type type = new TypeToken<List<NotificationGn>>(){}.getType();
-        List<NotificationGn> notificationgns = gson.fromJson(chaine, type);
+       Assert.assertTrue(notificationService.checkEventAndgenerateNotification(input));
 
-//        System.out.println(notificationgns);
+       NotificationInput input1 = new NotificationInput();
+       input1.setAction("update_project_phase");
+       input1.setEntityId(1);
+       input1.setEntityType("PROJET");
+       input1.setTitre("interconnexion des ministeres");
+       input1.setAttributName("phase actuelle");
+       input1.setLastVersion("pre_etude");
+       input1.setCurrentVersion("financement");
 
-        for (NotificationGn notificationgn : notificationgns) {
-            System.out.println(notificationgn.getLibelle());
-        }
-    }
+       Assert.assertTrue(notificationService.checkEventAndgenerateNotification(input1));
+   }
 
-    public static String readFileToString() throws IOException {
-        String filePath = "D:/Epub/eventDictionnary.json";
 
-        StringBuilder fileData = new StringBuilder(1000);//Constructs a string buffer with no characters in it and the specified initial capacity
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+    // @Test
+    // public void generateNotificationTest() throws FileNotFoundException, SQLException, IOException {
+        // String chaine = readFileToString();
+        // Gson gson = new Gson();
+        // Type type = new TypeToken<List<NotificationGn>>(){}.getType();
+        // List<NotificationGn> notificationgns = gson.fromJson(chaine, type);
 
-        char[] buf = new char[1024];
-        int numRead = 0;
-        while ((numRead = reader.read(buf)) != -1) {
-            String readData = String.valueOf(buf, 0, numRead);
-            fileData.append(readData);
-            buf = new char[1024];
-        }
+        // for (NotificationGn notificationgn : notificationgns) {
+            // System.out.println(notificationgn.getDiffusions());
+        // }
+    // }
 
-        reader.close();
+    // public static String readFileToString() throws IOException {
+        // String filePath = "D:/Epub/eventDictionnary.json";
 
-        String returnStr = fileData.toString();
-        return returnStr;
-    }
+        // StringBuilder fileData = new StringBuilder(1000);//Constructs a string buffer with no characters in it and the specified initial capacity
+        // BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        // char[] buf = new char[1024];
+        // int numRead = 0;
+        // while ((numRead = reader.read(buf)) != -1) {
+            // String readData = String.valueOf(buf, 0, numRead);
+            // fileData.append(readData);
+            // buf = new char[1024];
+        // }
+
+        // reader.close();
+
+        // String returnStr = fileData.toString();
+        // return returnStr;
+    // }
 }
